9f430d5666fec78c6b19e45f274cb5dc9317af65
XCour <zakariaa.aitamran@gmail.com>
2024-09-06 22:10:37 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,56 @@
+# Script for Uploading Text, Images and Files
+
+This is a Windows batch script designed for uploading text, images, and various file types to the online service 0x0.st. The script utilizes curl to handle the file uploads and provides direct URLs to the uploaded files.
+
+## Features
+
+- Upload text files
+- Upload images
+- Upload other file types (e.g., .txt, .pdf, .docx...)
+
+## Prerequisites
+
+- **Windows OS**: This script is designed for Windows environments.
+- **curl**: The script uses `curl`, which is included by default in Windows 10 (version 1803 and later) and Windows 11. If you are using an older version of Windows, you may need to install `curl` manually.
+
+## How to Use
+
+1. **Download the script**: Download the `UploadScript.bat` file to your computer.
+
+2. **Run the script**: Double-click on the `UploadScript.bat` file to run it.
+
+3. **Follow the prompts**:
+- **Select the type of content**: Choose between uploading text, images, or other file types.
+
+![Screenshot_1](https://github.com/user-attachments/assets/89cd2100-d4e8-4d98-9ff5-af0ee2012102)
+
+- **Provide file or text**: Depending on your choice, enter the text or the file path you want to upload.
+
+![Screenshot_9](https://github.com/user-attachments/assets/674093e2-93f7-458e-8a5f-6a27ccb860f0)
+
+- **Retrieve URL**: After uploading, the script will display a URL where you can access the uploaded content.
+
+![Screenshot_4](https://github.com/user-attachments/assets/0bd5a72e-769e-49f0-99b9-9f9d38463c8e)
+
+## Notes
+
+- **File Restrictions**: The script checks file types based on the platform's restrictions. Ensure that the file types you upload comply with these restrictions.
+
+![Screenshot_7](https://github.com/user-attachments/assets/db2d0b91-f7ae-4e83-850f-61eda0d55dfa)
+
+- **File Restrictions**: The script allows the upload of various file types, except for the following restricted file types:
+  - `.exe` (Executable files)
+  - `.rar` (Compressed archive files)
+  - `.jar` (Java archive files)
+
+- **Temporary Files**: The script creates temporary files for text uploads, which are deleted after the upload is completed.
+
+## Troubleshooting
+
+- **File not found**: Ensure the file path you provide is correct and that the file exists.
+
+![Screenshot_6](https://github.com/user-attachments/assets/302c31a7-0603-4f63-9445-e97ab7ceef4d)
+
+## Contact
+
+For any questions or issues, please open an issue in the GitHub repository.
---
+++ UploadScript.bat
@@ -0,0 +1,300 @@
+@echo off
+:TAMOROT
+setlocal enabledelayedexpansion
+
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+
+mode con: cols=64 lines=20
+color 0A
+
+:: Checking for curl installation
+
+where curl >nul 2>nul
+if errorlevel 1 (
+    echo curl is not installed. Please install curl to use this script.
+    endlocal
+    pause >nul
+    exit /b
+)
+
+:: Display welcome message
+
+cls
+echo.
+echo.
+echo       "    _____                               _   	"
+echo       "   |_   _|_ _ _ __ ___   ___  _ __ ___ | |_ 	"
+echo       "     | |/ _` | '_ ` _ \ / _ \| '__/ _ \| __|	"
+echo       "     | | (_| | | | | | | (_) | | | (_) | |_ 	"
+echo       "     |_|\__,_|_| |_| |_|\___/|_|  \___/ \__|	"
+echo.
+call :ColorText 0f "               Welcome to Text And File Uploader!" & echo;
+echo.
+echo.
+
+:: Prompt the user to select the type of content to upload
+
+echo Select the type of content you want to upload:
+echo.
+call :ColorText 0f "-"& call :ColorText 0C "1"& call :ColorText 0b ")"& call :ColorText 0F "upload text" & echo;
+call :ColorText 0f "-"& call :ColorText 0C "2"& call :ColorText 0b ")"& call :ColorText 0F "upload image"& echo;
+call :ColorText 0f "-"& call :ColorText 0C "3"& call :ColorText 0b ")"& call :ColorText 0F "upload other file types ("& call :ColorText 0e "e.g., .txt, .pdf, .docx...)" & echo;
+echo.
+call :ColorText 0A "Enter your choice"& call :ColorText 0F " ("& call :ColorText 0C " 1"& call :ColorText 0F ","& call :ColorText 0C " 2"& call :ColorText 0F ","& call :ColorText 0E " or"& call :ColorText 0C " 3"& call :ColorText 0F " )"
+set /p choice=: 
+echo.
+
+if "%choice%"=="1" (
+    goto upload_text
+) else if "%choice%"=="2" (
+    goto upload_image
+) else if "%choice%"=="3" (
+    goto upload_file
+) else (
+    echo Invalid choice. Click ENTER and select a valid option.
+    endlocal
+    pause >nul
+    goto TAMOROT
+)
+
+:upload_text
+cls
+mode con: cols=100 lines=14
+color 0A
+echo.
+echo.
+echo 			"		 _____ _______  _______ 		"
+echo 			"		|_   _| ____\ \/ /_   _|		"
+echo 			"		  | | |  _|  \  /  | |  		"
+echo 			"		  | | | |___ /  \  | |  		"
+echo 			"		  |_| |_____/_/\_\ |_|  		"
+echo.
+call :ColorText 0f "                                    Write the text you want to upload!" & echo;
+echo.
+echo.
+
+:: Prompt the user to enter the text they want to upload
+call :ColorText 0f "Enter the text you want to upload"
+set /p text= : 
+echo.
+
+:: Create a temporary file with the entered text
+
+set temp_file=temp.txt
+echo %text% > %temp_file%
+
+:: Upload the file to 0x0.st and retrieve the URL
+
+for /f "tokens=*" %%i in ('curl -F "file=@%temp_file%" http://0x0.st') do (
+    set url=%%i
+)
+
+:: Display the URL
+cls
+mode con: cols=66 lines=13
+color 0A
+echo.
+echo.
+echo 	"		 _   _ ____  _     		"
+echo 	"		| | | |  _ \| |    		"
+echo 	"		| | | | |_) | |    		"
+echo 	"		| |_| |  _ <| |___ 		"
+echo 	"		 \___/|_| \_\_____|		"
+echo.
+call :ColorText 0f "                 Welcome to Text And File Uploader!" & echo;
+echo.
+echo.
+call :ColorText 0f "Text uploaded successfully. URL"
+echo : %url%
+echo.
+
+:: Delete the temporary file
+
+if exist "%temp_file%" (
+    del "%temp_file%"
+)
+
+endlocal
+pause >nul
+goto TAMOROT
+
+:upload_image
+cls
+mode con: cols=100 lines=14
+color 0A
+echo.
+echo.
+echo 			"	 ___                            	"
+echo 			"	|_ _|_ __ ___   __ _  __ _  ___ 	"
+echo 			"	 | || '_ ` _ \ / _` |/ _` |/ _ \	"
+echo 			"	 | || | | | | | (_| | (_| |  __/	"
+echo 			"	|___|_| |_| |_|\__,_|\__, |\___|	"
+echo 			"	                     |___/      	"
+echo.
+call :ColorText 0f "                           You can upload any image " & call :ColorText 0b " with any extension!" & echo;
+echo.
+echo.
+
+:: Prompt the user to enter the path of the image they want to upload
+call :ColorText 0f "Enter the path of the Image you want to upload"
+set /p file_path= : 
+echo.
+
+:: Check if the file exists
+
+if not exist "%file_path%" (
+	echo.
+    echo File not found. Click ENTER and try again.
+    endlocal
+    pause >nul
+    goto TAMOROT
+)
+
+:: Upload the file to 0x0.st and retrieve the URL
+
+for /f "tokens=*" %%i in ('curl -F "file=@%file_path%" http://0x0.st') do (
+    set url=%%i
+)
+
+:: Display the URL
+
+cls
+mode con: cols=66 lines=13
+color 0A
+echo.
+echo.
+echo 	"		 _   _ ____  _     		"
+echo 	"		| | | |  _ \| |    		"
+echo 	"		| | | | |_) | |    		"
+echo 	"		| |_| |  _ <| |___ 		"
+echo 	"		 \___/|_| \_\_____|		"
+echo.
+call :ColorText 0f "                 Welcome to Text And File Uploader!" & echo;
+echo.
+echo.
+call :ColorText 0f "Image uploaded successfully. URL"
+echo : %url%
+echo.
+
+endlocal
+pause >nul
+goto TAMOROT
+
+:upload_file
+cls
+mode con: cols=100 lines=14
+color 0A
+echo.
+echo.
+echo 			"		 _____ ___ _     _____ 			"
+echo 			"		|  ___|_ _| |   | ____|			"
+echo 			"		| |_   | || |   |  _|  			"
+echo 			"		|  _|  | || |___| |___ 			"
+echo 			"		|_|   |___|_____|_____|			"
+echo.
+call :ColorText 0f "                             File types" & call :ColorText 0c " .exe, .rar," & call :ColorText 0a " and " & call :ColorText 0c " .jar" & call :ColorText 0f " are not allowed!" & echo;
+echo.
+echo.
+
+:: Prompt the user to enter the path of the file they want to upload
+call :ColorText 0f "Enter path the file"& call :ColorText 0e " (e.g., .txt, .pdf, .docx...)"
+set /p file_path= : 
+echo.
+
+:: Check if the file exists
+
+if not exist "%file_path%" (
+    echo File not found. Click ENTER and try again.
+    endlocal
+    pause >nul
+    goto TAMOROT
+)
+
+:: Check if the file type is allowed
+
+call :check_file_type "%file_path%"
+if errorlevel 1 (
+    echo File type not allowed. Click ENTER and try again.
+    endlocal
+    pause >nul
+    goto TAMOROT
+)
+
+:: Upload the file to 0x0.st and retrieve the URL
+
+for /f "tokens=*" %%i in ('curl -F "file=@%file_path%" http://0x0.st') do (
+    set url=%%i
+)
+
+if "%url%"=="" (
+    echo Failed to upload the file. Please try again.
+    endlocal
+    pause >nul
+    goto TAMOROT
+)
+
+:: Display the URL
+
+cls
+mode con: cols=66 lines=13
+color 0A
+echo.
+echo.
+echo 	"		 _   _ ____  _     		"
+echo 	"		| | | |  _ \| |    		"
+echo 	"		| | | | |_) | |    		"
+echo 	"		| |_| |  _ <| |___ 		"
+echo 	"		 \___/|_| \_\_____|		"
+echo.
+call :ColorText 0f "                 Welcome to Text And File Uploader!" & echo;
+echo.
+echo.
+call :ColorText 0f "File uploaded successfully. URL"
+echo : %url%
+echo.
+
+endlocal
+pause >nul
+goto TAMOROT
+
+:check_file_type
+
+:: Check if the file type is allowed
+
+set "file_path=%~1"
+set "allowed_extensions=pdf docx txt jpg jpeg png gif bmp dat"
+
+:: Extract file extension
+
+for %%A in ("%file_path%") do set "file_ext=%%~xA"
+set "file_ext=%file_ext:~1%"
+
+:: Check if the extension is allowed
+
+set "is_valid=0"
+for %%x in (%allowed_extensions%) do (
+    if /i "%file_ext%"=="%%x" (
+        set "is_valid=1"
+        goto :break
+    )
+)
+:break
+
+if "%is_valid%"=="1" (
+    exit /b 0
+) else (
+    exit /b 1
+)
+
+goto :Beginoffile
+
+:ColorText
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
+goto :eof
+
+:Beginoffile
